4854551024a193d594e4e9b6c49bdb9333a1ff17
moraissdev <renanbusinessdev@gmail.com>
2025-08-21 12:35:09 -0300

    commit

--- Revisão/Ponteiros/Udemy - Curso/Exercícios/ex30.c
+++ Revisão/Ponteiros/Udemy - Curso/Exercícios/ex30.c
@@ -8,7 +8,7 @@ int main() {
     int vet[4] = {2, 4, 6, 8};
     int *p = vet; //*p = 2
     *(p + 2) = *(p + 1) + 3; //*(p + 2) = 4 + 3 -> vet[2] = 7.
-    *(p + 1) = *(p) + 1; //*(p + 1) = 7 + 1 -> vet[3] = 8.
+    *(p + 1) = *(p) + 1; //*(p + 1) = 2 + 1 -> vet[1] = 3. -> *(p) acessa o índice 0 do vetor.
     printf("%d %d %d %d\n", vet[0], vet[1], vet[2], vet[3]);
     return 0;
 }
